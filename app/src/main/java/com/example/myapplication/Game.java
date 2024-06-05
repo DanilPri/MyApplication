35fb37d6097480eeb1e5eeaec9a3f464ab61dbfd
Daniil Pri <pridanser2007@gmail.com>
2024-06-05 18:41:25 +0400

    123

--- app/src/main/java/com/example/myapplication/Game.java
+++ app/src/main/java/com/example/myapplication/Game.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+import static com.example.myapplication.Variants.var1;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,21 +18,40 @@ public class Game {
         board = new int[height][width];
         for (int i = 0; i < this.height; i++) {
             for (int j = 0; j < this.width; j++) {
-                board[i][j] = Variants.var1[i][j];
+                board[i][j] = var1[i][j];
             }
         }
     }
 
     public ArrayList<ArrayList<Integer>> Game1() {
         ArrayList <ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        int x1 = 0;
         Game g = MainActivity.g;
         for (int i = 0; i < g.height; i++) {
             ArrayList<Integer> line = new ArrayList<Integer>();
+            Integer k = 0;
+            Boolean flag = false;
             for (int j = 0; j < g.width; j++) {
-                if (j==0);
-                x1++;
+                if (this.board[i][j]==0){
+                    if(flag){
+                        flag = false;
+                        line.add(k);
+                        k = 0;
+                    }
+                }
+                if (this.board[i][j]==1){
+                    if(flag){
+                        k++;
+                    }
+                    else{
+                        flag = true;
+                        k = 1;
+                    }
+                }
 
+
+            }
+            if (flag) {
+                line.add(k);
             }
             result.add(line);
         }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(R.layout.activity_main);
         g = new Game(10, 10);
         setContentView(new MyDraw(this));
+        ArrayList <ArrayList<Integer>> result = g.Game1();
+        String lg = "";
+        Log.d("game111",g.Game1().get(0).toString());
+
+
     }
 }
